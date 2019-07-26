bb637d500ad95a47b0d56b146ff03f8d80061479
George Ruiz <georuiz817@gmail.com>
2019-07-26 02:23:49 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,6 +1,10 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
   
-  
+    def change
+  	change_column :grade, :integer 
+  	add_column :birthdate, :string
+    
+  end
   
   
   
