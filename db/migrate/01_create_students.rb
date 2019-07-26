d3516f64b6a6e8b3cd122089ae00c33f3703ae23
George Ruiz <georuiz817@gmail.com>
2019-07-26 02:12:31 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,17 +1,15 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
  def change
-4.	  def change
-5.	    create_table :artists do |t|
-6.	      t.string :name
-7.	      t.string :genre
-8.	      t.integer :age
-9.	      t.string :hometown
-10.	    end
-11.	  end
-12.	end
+ create_table :artists do |t|
+   t.string :name
+   t.string :genre
+   t.integer :age
+   t.string :hometown
+	    end
+	  end
+	end
 
- end
- end
+
 
 
 
