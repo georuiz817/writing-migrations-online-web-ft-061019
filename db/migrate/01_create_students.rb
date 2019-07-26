692beee3f884ef6c2cf9b4c795f4f1852c1d0a04
George Ruiz <georuiz817@gmail.com>
2019-07-26 02:11:43 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,9 +1,8 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
  def change
- add_column :artists, :favorite_food, :string
+ create_table :artists, :favorite_food, :string
  end
  end
 
 
 
-end
